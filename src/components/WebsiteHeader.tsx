3e243d3f7fa10da1988adccbcd9a97ea523efbb7
l.engelhardt <l.engelhardt@edu.letteverein.berlin>
2024-06-27 13:45:48 +0200

    Path changes and Color / font setup

--- src/components/WebsiteHeader.tsx
+++ src/components/WebsiteHeader.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../scss/WebsiteHeader.scss";
+import "../scss/Components/WebsiteHeader.scss";
 
 const WebsiteHeader: React.FC = () => {
   return (
