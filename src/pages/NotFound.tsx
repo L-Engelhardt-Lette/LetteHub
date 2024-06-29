c54b1a6aa6ff69f7088bf7db3cd109afba6b1698
lede <l.engelhardt@edu.letteverein.berlin>
2024-06-29 18:44:53 +0200

    404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,12 +2,23 @@
 
 import React from "react";
 import "../scss/pages/NotFound.scss";
+import { GoCloudOffline } from "react-icons/go";
 
 const NotFound: React.FC = () => {
   return (
     <div className="not-found">
-      <h1>404</h1>
-      <p>Not Found</p>
+      <div id="container">
+        <div id="title-container">
+          <GoCloudOffline id="icon" />
+          <h1 id="title">
+            4 <span id="zero">0</span> 4 Error
+          </h1>
+        </div>
+        <h2 id="undertitle">Not Found</h2>
+        <p id="text">
+          Oops! It looks like you've stumbled onto a page that doesn't exist.
+        </p>
+      </div>
     </div>
   );
 };
