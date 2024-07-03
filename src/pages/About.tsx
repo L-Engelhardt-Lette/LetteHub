471b88c2ba38da3cad2949fffd416a2df2be2e2e
Quaxinator <ole.j.herold@gmail.com>
2024-07-03 13:54:42 +0200

    register test

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Register from "../components/test/Register";
 
 function AboutPage() {
   return (
     <div>
       <h1>About Us</h1>
       {/* Your about page content */}
+      <Register />
     </div>
   );
 }
