498adf809332aebf3a45e672e76a2e61cbe32fa0
l.engelhardt <l.engelhardt@edu.letteverein.berlin>
2024-07-04 12:00:55 +0200

    backend user login

--- BACKEND/src/routes/authRoutes.ts
+++ BACKEND/src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { register, login } from "../controllers/authController";
+import { register, login, getUserDetails } from "../controllers/authController";
+import authMiddleware from "../middleware/authMiddleware"; // Middleware to check authentication
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/user", authMiddleware, getUserDetails); // Add this route
 
 export default router;
