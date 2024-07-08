54888f7ef299342fb9070a9c3221cc2a5a436e80
l.engelhardt <l.engelhardt@edu.letteverein.berlin>
2024-07-08 12:55:26 +0200

    color wheel and save button

--- src/components/Project/ColorChangeButton.tsx
+++ src/components/Project/ColorChangeButton.tsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../scss/Components/Project/ColorChangeButton.scss"; // Ensure this path points to your SCSS file
-import { FaPaintbrush } from "react-icons/fa6";
+import { FaPaintBrush } from "react-icons/fa";
+import { SketchPicker } from "react-color";
 
-const TooltipButton = () => {
+const TooltipButton = ({ currentColor, onColorChange }) => {
+  const [showColorPicker, setShowColorPicker] = useState(false);
+  const [temporaryColor, setTemporaryColor] = useState(currentColor);
+
+  const handleClick = () => {
+    setShowColorPicker(!showColorPicker);
+  };
+
+  const handleColorChange = (color) => {
+    setTemporaryColor(color.hex);
+  };
+
+  const handleSave = () => {
+    onColorChange(temporaryColor);
+    setShowColorPicker(false);
+  };
+
   return (
-    <div className="TooltipButtonContainer">
+    <div className="relative TooltipButtonContainer">
       <div className="flex-container" data-tooltip="Color Change">
-        <FaPaintbrush id="icon" />
+        <FaPaintBrush id="icon" onClick={handleClick} />
       </div>
+      {showColorPicker && (
+        <div className="absolute z-20 mt-2">
+          <SketchPicker
+            color={temporaryColor}
+            onChangeComplete={handleColorChange}
+          />
+          <button
+            onClick={handleSave}
+            className="mt-2 p-2 bg-blue-500 text-white rounded"
+          >
+            Save
+          </button>
+        </div>
+      )}
     </div>
   );
 };
