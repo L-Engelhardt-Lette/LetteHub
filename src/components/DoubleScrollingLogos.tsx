7fabd1e63c3601dc733251106c470c42a9e02c4b
l.engelhardt <l.engelhardt@edu.letteverein.berlin>
2024-07-09 10:45:10 +0200

    updated logos

--- src/components/DoubleScrollingLogos.tsx
+++ src/components/DoubleScrollingLogos.tsx
@@ -1,32 +1,33 @@
 import { motion } from "framer-motion";
 import {
-  SiNike,
-  Si3M,
-  SiAbstract,
-  SiAdobe,
-  SiAirtable,
-  SiAmazon,
-  SiBox,
-  SiBytedance,
-  SiChase,
-  SiCloudbees,
-  SiBurton,
-  SiBmw,
-  SiHeroku,
-  SiBuildkite,
-  SiCouchbase,
-  SiDailymotion,
-  SiDeliveroo,
-  SiEpicgames,
-  SiGenius,
-  SiGodaddy,
+  SiTypescript,
+  SiJavascript,
+  SiXampp,
+  SiVite,
+  SiExpress,
+  SiAdobeillustrator,
 } from "react-icons/si";
+import { GrMysql } from "react-icons/gr";
+import {
+  FaReact,
+  FaCss3,
+  FaHtml5,
+  FaNode,
+  FaGithub,
+  FaMarkdown,
+} from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { BsFiletypeScss, BsFiletypeJson, BsFiletypeTsx } from "react-icons/bs";
+import { VscVscode } from "react-icons/vsc";
+import { AiOutlineOpenAI } from "react-icons/ai";
+import { DiScrum } from "react-icons/di";
 import { IconType } from "react-icons";
+import React from "react";
 
 const DoubleScrollingLogos = () => {
   return (
     <section className="bg-white py-4">
-      <div className="flex  overflow-hidden">
+      <div className="flex overflow-hidden">
         <TranslateWrapper>
           <LogoItemsTop />
         </TranslateWrapper>
@@ -71,10 +72,10 @@ const TranslateWrapper = ({
   );
 };
 
-const LogoItem = ({ Icon }: { Icon: IconType }) => {
+const LogoItem = ({ Icon, href }: { Icon: IconType; href: string }) => {
   return (
     <a
-      href="/"
+      href={href}
       rel="nofollow"
       target="_blank"
       className="w-16 md:w-24 h-16 md:h-24 flex justify-center items-center hover:bg-slate-200 text-black transition-colors"
@@ -86,31 +87,46 @@ const LogoItem = ({ Icon }: { Icon: IconType }) => {
 
 const LogoItemsTop = () => (
   <>
-    <LogoItem Icon={SiNike} />
-    <LogoItem Icon={Si3M} />
-    <LogoItem Icon={SiAbstract} />
-    <LogoItem Icon={SiAdobe} />
-    <LogoItem Icon={SiAirtable} />
-    <LogoItem Icon={SiAmazon} />
-    <LogoItem Icon={SiBox} />
-    <LogoItem Icon={SiBytedance} />
-    <LogoItem Icon={SiChase} />
-    <LogoItem Icon={SiCloudbees} />
+    <LogoItem Icon={SiTypescript} href="https://www.typescriptlang.org/" />
+    <LogoItem Icon={GrMysql} href="https://www.mysql.com/" />
+    <LogoItem Icon={FaReact} href="https://reactjs.org/" />
+    <LogoItem Icon={SiJavascript} href="https://www.javascript.com/" />
+    <LogoItem Icon={SiXampp} href="https://www.apachefriends.org/index.html" />
+    <LogoItem Icon={VscVscode} href="https://code.visualstudio.com/" />
+    <LogoItem
+      Icon={FaHtml5}
+      href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5"
+    />
+    <LogoItem Icon={SiVite} href="https://vitejs.dev/" />
+    <LogoItem Icon={FaNode} href="https://nodejs.org/" />
+    <LogoItem Icon={SiExpress} href="https://expressjs.com/" />
   </>
 );
 
 const LogoItemsBottom = () => (
   <>
-    <LogoItem Icon={SiBmw} />
-    <LogoItem Icon={SiBurton} />
-    <LogoItem Icon={SiBuildkite} />
-    <LogoItem Icon={SiCouchbase} />
-    <LogoItem Icon={SiDailymotion} />
-    <LogoItem Icon={SiDeliveroo} />
-    <LogoItem Icon={SiEpicgames} />
-    <LogoItem Icon={SiGenius} />
-    <LogoItem Icon={SiGodaddy} />
-    <LogoItem Icon={SiHeroku} />
+    <LogoItem
+      Icon={FaCss3}
+      href="https://developer.mozilla.org/en-US/docs/Web/CSS"
+    />
+    <LogoItem Icon={RiTailwindCssFill} href="https://tailwindcss.com/" />
+    <LogoItem Icon={BsFiletypeScss} href="https://sass-lang.com/" />
+    <LogoItem Icon={BsFiletypeJson} href="https://www.json.org/json-en.html" />
+    <LogoItem
+      Icon={BsFiletypeTsx}
+      href="https://www.typescriptlang.org/docs/handbook/jsx.html"
+    />
+    <LogoItem
+      Icon={SiAdobeillustrator}
+      href="https://www.adobe.com/products/illustrator.html"
+    />
+    <LogoItem Icon={FaGithub} href="https://github.com/" />
+    <LogoItem
+      Icon={FaMarkdown}
+      href="https://daringfireball.net/projects/markdown/"
+    />
+    <LogoItem Icon={AiOutlineOpenAI} href="https://www.openai.com/" />
+    <LogoItem Icon={DiScrum} href="https://www.scrum.org/" />
   </>
 );
 
