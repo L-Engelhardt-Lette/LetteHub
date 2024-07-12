7b139f902a64eca89e1349c8a3646b18f2d91085
l.engelhardt <l.engelhardt@edu.letteverein.berlin>
2024-07-12 12:53:31 +0200

    wir haben vielleicht einen loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React, { useState } from "react";
+// src/App.tsx
+import React, { useState, useEffect } from "react";
 import "./scss/App.scss";
 import WebsiteHeader from "./components/WebsiteHeader";
+import Loader from "./components/Loader";
 import "./styles.scss";
 
-function App() {
-  return (
-    <>
-      <WebsiteHeader />
-    </>
-  );
-}
+const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate an API call
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  return <>{loading ? <Loader /> : <WebsiteHeader />}</>;
+};
 
 export default App;
