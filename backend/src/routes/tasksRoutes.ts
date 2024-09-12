410a4f22ffaaa68a99070e962fac016eeae6b1a6
Ludwig LEDE <ludwig.engelhardt@gmail.com>
2024-09-12 13:05:43 +0200

    redo of backend

--- backend/src/routes/tasksRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-
-const router = Router();
-
-// Define your task-related routes here
-router.get("/", (req, res) => res.send("Task route"));
-
-export default router;
