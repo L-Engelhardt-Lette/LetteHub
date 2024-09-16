66490325451c971877466bcaf027eeddd7fe2b55
Ludwig LEDE <ludwig.engelhardt@gmail.com>
2024-09-16 14:56:25 +0200

    moved backend

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,45 @@
+// models/user.model.ts
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../../../backend/src/database"; // Import your Sequelize instance
+
+class User extends Model {
+  public id!: number;
+  public username!: string;
+  public email!: string;
+  public password!: string;
+  public role!: string;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "user",
+    },
+  },
+  {
+    sequelize,
+    tableName: "users",
+  }
+);
+
+export default User;
